40bca4268e5891325a41cc3dbffe110cb9a590fa
dirtinyoureyes <90673091+dirtinyoureyes@users.noreply.github.com>
2021-12-21 13:45:32 +0800

    Update and rename print_variable to print_variable.c

--- print_variable.c
+++ print_variable.c
@@ -3,7 +3,7 @@
 int main() {
 
     int b = 87;
-    for (int j = 5; j < 23; j++) {
+    for (int j = 5; j < 23; j++)
         //give me some output messages in following format
         //and that can be done by add only ONE line code.
         //please refer to Google slides, page 14.
@@ -14,5 +14,5 @@ int main() {
         //j=9, b=783
         //...
         //j=22, b=1914
+        printf("j=%d, b=%d\n", j, b*j);
     }
-}
